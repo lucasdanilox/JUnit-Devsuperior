e13c12fe27fed9b36a530cf2eb71b32873d841d4
lucasdanilox <lukastecno@gmail.com>
2023-12-29 18:09:26 -0300

    Class Account, Tests JUnit

---
+++ src/test/entities/AccountTests.java
@@ -0,0 +1,24 @@
+package test.entities;
+
+import entities.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+
+    @Test
+    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+
+        double amount = 200.0;
+        double expectedValue = 196.0;
+        Account acc = new Account(1L, 0.0);
+
+        acc.deposit(200.0);
+
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+
+
+    }
+
+
+}
