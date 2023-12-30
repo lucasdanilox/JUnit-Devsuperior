e700318bc1a070790f1fc69ab69c40c6c020b388
lucasdanilox <lukastecno@gmail.com>
2023-12-29 23:51:01 -0300

    Withdraw

--- src/test/entities/AccountTests.java
+++ src/test/entities/AccountTests.java
@@ -47,5 +47,23 @@ public class AccountTests {
         Assertions.assertTrue(result == initialBalance);
     }
 
+    @Test
+    public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
 
+        Account acc = AccountFactory.createAccount(800.0);
+
+        acc.withdraw(500.0);
+
+        Assertions.assertEquals(300, acc.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Account acc = AccountFactory.createAccount(800.0);
+
+            acc.withdraw(801.0);
+        });
+    }
 }
