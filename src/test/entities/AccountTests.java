1300dee0e9b5c89dc163429c8cac4ab2b1807a66
lucasdanilox <lukastecno@gmail.com>
2023-12-29 23:41:50 -0300

    Total withdraw

--- src/test/entities/AccountTests.java
+++ src/test/entities/AccountTests.java
@@ -34,5 +34,18 @@ public class AccountTests {
 
     }
 
+    @Test
+    public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+
+        double expectedValue = 0.0;
+        double initialBalance = 800.0;
+        Account acc = AccountFactory.createAccount(initialBalance);
+
+        double result = acc.fullWithdraw();
+
+        Assertions.assertTrue(expectedValue == acc.getBalance());
+        Assertions.assertTrue(result == initialBalance);
+    }
+
 
 }
