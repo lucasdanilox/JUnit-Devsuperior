ca80166a4452ed817257f68a3a5a97973515876b
lucasdanilox <lukastecno@gmail.com>
2023-12-29 23:29:38 -0300

    Factory pattern

--- src/test/entities/AccountTests.java
+++ src/test/entities/AccountTests.java
@@ -3,6 +3,7 @@ package test.entities;
 import entities.Account;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 
@@ -11,7 +12,7 @@ public class AccountTests {
 
         double amount = 200.0;
         double expectedValue = 196.0;
-        Account acc = new Account(1L, 0.0);
+        Account acc = AccountFactory.createEmptyAccount();
 
         acc.deposit(200.0);
 
@@ -24,7 +25,7 @@ public class AccountTests {
     public void depositShouldDoNothingWhenNegativeAmount() {
 
         double expectedValue = 100.0;
-        Account acc = new Account(1L, expectedValue);
+        Account acc = AccountFactory.createAccount(expectedValue);
         double amount = -200.0;
 
         acc.deposit(amount);
